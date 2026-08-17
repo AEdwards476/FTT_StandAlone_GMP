185d1878e1625f98a2036a404316eddbaad892a8
Cormac Lynch <122904574+cormacmlynch@users.noreply.github.com>
2026-08-17 11:38:00 +0100

    Add fossil gas price shock 2036
    Based on DESNZ 'C' trajectory and modelled off 2022 gas price spike.

--- workflow.bat
+++ workflow.bat
@@ -1,4 +1,4 @@
-python Utilities/monte_carlo.py generate --base-scenario S0,nat_scale
-python Utilities/monte_carlo.py run --base-scenario S0,nat_scale
-python figures/make_figures.py --base-scenario S0,nat_scale --format svg
-python figures/make_figures.py --base-scenario S0,nat_scale --format png
+python Utilities/monte_carlo.py generate --base-scenario S0,nat_scale,gas_shock_loc,gas_shock_nat --n 100
+python Utilities/monte_carlo.py run --base-scenario S0,nat_scale,gas_shock_loc,gas_shock_nat
+python figures/make_figures.py --base-scenario S0,nat_scale,gas_shock_loc,gas_shock_nat --format svg
+python figures/make_figures.py --base-scenario S0,nat_scale,gas_shock_loc,gas_shock_nat --format png
